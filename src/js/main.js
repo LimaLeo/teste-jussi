5cc8379d37756ce3d6ed2cd51f4705879f54abea
Leonardo Lima <leonardotlima2@gmail.com>
2017-03-10 16:43:03 -0300

    finalizacao layout

--- src/js/main.js
+++ src/js/main.js
@@ -2,18 +2,33 @@ var ordenaParada = function(){
 	
 }
 
+var moveElevador = function(botao, alturaAndar){
+	alturaElevador = $("#elevador").offset().top;
+	deslocamento = alturaElevador - alturaAndar -5;
+	botao.toggleClass("active");
+
+	console.log(deslocamento);
+	$("#elevador").animate({
+		"top": "-=" + deslocamento + "px" }, 1000, function(){
+			botao.toggleClass("active");
+			$("#elevador").find(".porta").toggleClass("aberta");
+			setTimeout(function(){
+    			$("#elevador").find(".porta").toggleClass("aberta");
+    		},1500);
+		}
+	);
+}
+
 $(function(){
 	$(".sobe").on("click", function(event){
 		event.preventDefault();
-		$("#elevador").animate({
-			"top": "-=109px" }, "slow" 
-		);
+		alturaAndar = $(this).closest("section").find("article").offset().top;
+		moveElevador($(this), alturaAndar);
 	});
 
 	$(".desce").on("click", function(event){
 		event.preventDefault();
-		$("#elevador").animate({
-			"top": "+=109px" }, "slow" 
-		);
+		alturaAndar = $(this).closest("section").find("article").offset().top;
+		moveElevador($(this), alturaAndar);
 	});
 });
