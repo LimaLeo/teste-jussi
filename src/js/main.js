5341e2aa036882ad3951f38f7cc3ae62e31edcfa
Leonardo Lima <leonardotlima2@gmail.com>
2017-03-11 13:23:38 -0300

    implementacao elevador

--- src/js/main.js
+++ src/js/main.js
@@ -1,34 +1,90 @@
-var ordenaParada = function(){
-	
+var listAndares = [];
+
+var ordenaParadaCrescente = function(){
+	listAndares.sort(function(a,b) {
+        return a[0]-b[0];
+    });
 }
 
-var moveElevador = function(botao, alturaAndar){
-	alturaElevador = $("#elevador").offset().top;
-	deslocamento = alturaElevador - alturaAndar -5;
-	botao.toggleClass("active");
+var ordenaParadaDecrescente = function(){
+	listAndares.sort(function(a,b) {
+        return a[0]-b[0];
+    });
+    listAndares.reverse();
+}
 
-	console.log(deslocamento);
-	$("#elevador").animate({
-		"top": "-=" + deslocamento + "px" }, 1000, function(){
-			botao.toggleClass("active");
+var incluiParada = function(botao){
+	andar = botao.closest('section'); 
+	numeroAndar = andar.closest("section").find(".title").text().replace(/[^0-9]/g,'');
+	calculaDeslocamento(botao);
+	listAndares.push([deslocamento,botao]);
+	
+	alturaAndar = botao.closest("section").find("article").offset().top;
+	alturaElevador = $("#elevador").offset().top;
+	if(alturaAndar>alturaElevador)
+	{
+		ordenaParadaDecrescente();
+	}else{
+		ordenaParadaCrescente();
+	}
+
+	decideDestino();
+}
+
+var decideDestino = function(){
+	botao = listAndares[0][1];
+	moveElevador(botao);
+}
+
+var calculaDeslocamento = function(botao){
+	alturaElevador = $("#elevador").offset().top;
+	alturaAndar = botao.closest("section").find("article").offset().top;
+	deslocamento = alturaElevador - alturaAndar -5;
+	return deslocamento;
+}
+
+var statusAndar = function(botao){
+	botao.closest('section').toggleClass("active");
+	$("#elevador").find(".porta").toggleClass("aberta");
+}
+
+var fimDePercurso = function(){
+	$("#elevador").stop().animate({
+		"top": "-=" + calculaDeslocamento($(".terreo .sobe")) + "px" }, 5000, function(){
 			$("#elevador").find(".porta").toggleClass("aberta");
+			$(".terreo").closest("section").toggleClass("active");
 			setTimeout(function(){
-    			$("#elevador").find(".porta").toggleClass("aberta");
-    		},1500);
+				$("#elevador").find(".porta").toggleClass("aberta");
+			}, 3000);
+	});
+}
+
+var moveElevador = function(botao){
+	calculaDeslocamento(botao);
+	$("#elevador").stop().animate({
+		"top": "-=" + deslocamento + "px" }, 5000, function(){
+			statusAndar(botao);
+			$(".terreo").closest("section").toggleClass("active");
+			setTimeout(function(){
+				statusAndar(botao);
+				setTimeout(function(){
+		    		listAndares.shift();
+		    		if(listAndares.length)
+		    		{
+		    			decideDestino(botao);
+		    		}else{
+		    			fimDePercurso();
+		    		}
+	    		},1000);
+    		},3000);
 		}
 	);
 }
 
 $(function(){
-	$(".sobe").on("click", function(event){
+	$("aside a").on("click", function(event){
+		var andar = $(this).closest('section'); 
 		event.preventDefault();
-		alturaAndar = $(this).closest("section").find("article").offset().top;
-		moveElevador($(this), alturaAndar);
-	});
-
-	$(".desce").on("click", function(event){
-		event.preventDefault();
-		alturaAndar = $(this).closest("section").find("article").offset().top;
-		moveElevador($(this), alturaAndar);
+		incluiParada($(this));
 	});
 });
